d7525293b274be30d85734aadc5ce2a596aa03e1
josephluai <cjluaite@gmail.com>
2017-12-14 09:11:06 -0500

    It's fixed the issue. Now you can navigate to another pages.

--- app/src/main/java/com/example/josephluai/qrcode/MainActivity.java
+++ app/src/main/java/com/example/josephluai/qrcode/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.josephluai.qrcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    Button btn2;
+    public void init(){
+        btn1= (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toy = new Intent(MainActivity.this,ReaderActivity.class);
+                startActivity(toy);
+            }
+        });
+    }
+    public void inti(){
+        btn2= (Button)findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent soy = new Intent(MainActivity.this,GeneratorActivity.class);
+                startActivity(soy);
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        inti();
     }
 }
